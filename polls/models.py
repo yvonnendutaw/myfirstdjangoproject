9f7bf4a96efd75d1274ca50483ae44ad86b22fc2
yvonne <yvonne@netwookie.org>
2016-11-22 12:59:54 +0300

    added style

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -17,8 +18,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() \
-            - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now -datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -29,5 +30,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
 
 
